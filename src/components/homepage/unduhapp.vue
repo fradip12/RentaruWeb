f31fce304db2c3887cf8ec0ece3ffa3dc5896822
Aufa Nabil <aufa.nabil.amiri@gmail.com>
2020-07-06 10:40:31 +0700

    ui : homepage, unduhapp responsiveness

--- src/components/homepage/unduhapp.vue
+++ src/components/homepage/unduhapp.vue
@@ -1,54 +1,28 @@
 <template>
-  <v-container fluid class="pa-0">
+  <v-container fluid class="pa-sm-0 pa-4">
     <v-row no-gutters>
-      <img src="../../assets/apple-hand.png" class="hand-img" />
+      <img
+        v-if="width >= 600"
+        src="../../assets/apple-hand.png"
+        class="hand-img"
+      />
 
       <v-col class="d-flex flex-column justify-center">
-        <div class="text-lg-h2 font-weight-regular secondary--text">
+        <div
+          class="text-md-h3 text-h4 text-center text-sm-left font-weight-regular secondary--text"
+        >
           Unduh Aplikasi rentaru !
         </div>
-        <div class="text-lg-h5 my-10 secondary--text">
+        <div
+          class="text-lg-h5 my-md-10 my-2 text-center text-sm-left secondary--text"
+        >
           Temukan kemudahan untuk sewa barang dan jasa, semua dalam satu
           aplikasi di genggaman anda.
         </div>
         <v-container>
-          <v-row>
-            <v-btn
-              dark
-              class="pa-8 ma-5 rounded-lg"
-              width="260px"
-              height="80px"
-            >
-              <v-container>
-                <v-row>
-                  <v-icon size="50">fab fa-apple</v-icon>
-                  <v-col class="justify-start ml-4">
-                    <div class="text-left text-subtitle-2">
-                      Dapatkan di<br />
-                      <span class="text-left text-h5 pa-0">App Store</span>
-                    </div>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-btn>
-            <v-btn
-              dark
-              class="pa-8 ma-5 rounded-lg"
-              width="260px"
-              height="80px"
-            >
-              <v-container>
-                <v-row>
-                  <v-icon size="50">fab fa-google-play</v-icon>
-                  <v-col class="justify-start">
-                    <div class="text-left text-subtitle-2">
-                      Dapatkan di<br />
-                      <span class="text-left text-h5 pa-0">Google Play</span>
-                    </div>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-btn>
+          <v-row class="justify-center justify-sm-start">
+            <unduhBtn iconName="fab fa-apple" marketText="App Store" />
+            <unduhBtn iconName="fab fa-google-play" marketText="Google Play" />
           </v-row>
         </v-container>
       </v-col>
@@ -57,7 +31,19 @@
 </template>
 
 <script>
+import unduhBtn from "./unduhbtn";
+
 export default {
-  name: "UnduhApp"
+  name: "UnduhApp",
+
+  components: {
+    unduhBtn
+  },
+
+  computed: {
+    width() {
+      return screen.width;
+    }
+  }
 };
 </script>
---
+++ src/components/homepage/unduhbtn.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <v-btn
+    dark
+    class="pa-8 ma-5 ma-sm-0 mr-sm-5 mr-md-5 ma-md-5 rounded-lg"
+    :width="width <= 600 ? '230px' : width <= 960 ? '171px' : '260px'"
+    :height="width <= 600 ? '30px' : width <= 960 ? '19px' : '80px'"
+  >
+    <v-container>
+      <v-row no-gutters>
+        <v-icon :size="width <= 600 ? '30' : width <= 960 ? '40' : '50'">{{
+          iconName
+        }}</v-icon>
+        <v-col class="justify-start ml-4 ml-md-4 ml-sm-2">
+          <div class="text-left text-md-subtitle-2 text-caption">
+            Dapatkan di<br />
+            <span class="text-left text-md-h5 text-sm-body-1 text-h6 pa-0">{{
+              marketText
+            }}</span>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "unduhBtn",
+
+  props: {
+    iconName: String,
+    marketText: String
+  },
+
+  computed: {
+    width() {
+      return screen.width;
+    }
+  }
+};
+</script>
