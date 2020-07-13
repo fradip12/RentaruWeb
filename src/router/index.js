f448b951fc91cd0fbe0b6d4f1781fadde41940f9
Aufa Nabil <aufa.nabil.amiri@gmail.com>
2020-07-13 10:05:50 +0700

    refactor : ubah underconstruct dan 404 jadi page

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     name: 'Profile',
     component: () => import('@/views/profile'),
   },
+  {
+    path: '/error404',
+    name: 'Error 404',
+    component: () => import('@/views/unknown404'),
+  },
+  {
+    path: '/underconstruct',
+    name: 'Under Construct',
+    component: () => import('@/views/underconstruct'),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/unknown404.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-main>
+        <v-container class="pt-2 pb-2">
+          <br>
+          <br>
+          <br>
+          <center>
+            <img src="https://www.vippng.com/png/full/375-3750100_404-404-png.png" height="45%" width="45%">
+          </center>
+          <br>
+            <div style="text-align:center">
+              <h1 style="color:#ee4000">Halaman yang Anda cari tidak ditemukan.</h1>
+              <br>
+              <br>
+              <v-btn rounded color="#ee4000" style="color:#FFFFFF" depressed> Kembali ke Beranda </v-btn>
+              <br>
+            </div>
+        </v-container>
+    </v-main>
+</template>
