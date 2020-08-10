f68cc0ce5b568fadb8ee3857b6a51c13e4c215b8
Aufa Nabil <aufa.nabil.amiri@gmail.com>
2020-08-10 09:11:02 +0700

    refactor : remove unnecessary class

--- src/components/iklanb2b.vue
+++ src/components/iklanb2b.vue
@@ -12,37 +12,11 @@
 </template>
 
 <style scoped>
-.container-relative {
-  position: relative;
-}
-
 .top-image {
   width: 100%;
   height: 23vw;
   max-height: 700px;
   object-fit: cover;
-}
-
-.top-text {
-  position: absolute;
-  bottom: 6px;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.4);
-  color: white;
-  padding: 20px 40px;
-}
-
-.top-rating {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  flex-direction: row;
-}
-
-@media screen and (max-width: 1264px) {
-  .top-text {
-    padding: 10px 20px;
-  }
 }
 </style>
 
