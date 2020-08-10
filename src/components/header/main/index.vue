7d03cdfe1e44830f6c4dc0b8fff0f4e6f9c48962
Aufa Nabil <aufa.nabil.amiri@gmail.com>
2020-08-10 10:44:03 +0700

    new : submenu

--- src/components/header/main/index.vue
+++ src/components/header/main/index.vue
@@ -6,114 +6,7 @@
           RENTARU
         </h1>
 
-        <v-menu
-          v-for="button in tombol"
-          :close-on-content-click="content_click_option"
-          :key="button"
-        >
-          <template #activator="{ on: menu }" v-show="button.options">
-            <v-btn color="info" icon small flat v-on="{ ...tooltip, ...menu }">
-              <v-icon>{{ button.icon }}</v-icon>
-            </v-btn>
-          </template>
-          <temmplate v-if="content_click_option" v-show="!button.options">
-            <v-tooltip bottom>
-              <template #activator="{ on: tooltip }">
-                <v-btn color="info" icon small flat v-on="{ ...tooltip }">
-                  <v-icon>{{ button.icon }}</v-icon>
-                </v-btn>
-              </template>
-              <span>{{ button.title }}</span>
-            </v-tooltip>
-          </temmplate>
-          <v-list v-show="button.options">
-            <v-list-tile
-              v-for="item in button.options"
-              :key="item.id"
-              @click="
-                item.options
-                  ? (content_click_option = false)
-                  : (content_click_option = true)
-              "
-            >
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-              </v-list-tile-content>
-              <v-list-tile-action v-if="item.options">
-                <v-menu
-                  v-model="menu"
-                  :close-on-content-click="false"
-                  :nudge-width="200"
-                  offset-x
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-icon v-on="on" color="info" small @click="menu = true"
-                      >fa-chevron-right</v-icon
-                    >
-                  </template>
-
-                  <v-card>
-                    <v-list>
-                      <v-list-tile avatar>
-                        <v-list-tile-avatar>
-                          <img
-                            src="https://cdn.vuetifyjs.com/images/john.jpg"
-                            alt="John"
-                          />
-                        </v-list-tile-avatar>
-
-                        <v-list-tile-content>
-                          <v-list-tile-title>John Leider</v-list-tile-title>
-                          <v-list-tile-sub-title
-                            >Founder of Vuetify.js</v-list-tile-sub-title
-                          >
-                        </v-list-tile-content>
-
-                        <v-list-tile-action>
-                          <v-btn
-                            :class="fav ? 'red--text' : ''"
-                            icon
-                            @click="fav = !fav"
-                          >
-                            <v-icon>favorite</v-icon>
-                          </v-btn>
-                        </v-list-tile-action>
-                      </v-list-tile>
-                    </v-list>
-
-                    <v-divider></v-divider>
-
-                    <v-list>
-                      <v-list-tile>
-                        <v-list-tile-action>
-                          <v-switch v-model="message" color="purple"></v-switch>
-                        </v-list-tile-action>
-                        <v-list-tile-title>Enable messages</v-list-tile-title>
-                      </v-list-tile>
-
-                      <v-list-tile>
-                        <v-list-tile-action>
-                          <v-switch v-model="hints" color="purple"></v-switch>
-                        </v-list-tile-action>
-                        <v-list-tile-title>Enable hints</v-list-tile-title>
-                      </v-list-tile>
-                    </v-list>
-
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-
-                      <v-btn flat @click="menu = false">Cancel</v-btn>
-                      <v-btn color="primary" flat @click="menu = false"
-                        >Save</v-btn
-                      >
-                    </v-card-actions>
-                  </v-card>
-                </v-menu>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-        </v-menu>
-        <!-- <v-menu open-on-hover offset-y :close-on-content-click="false">
+        <v-menu open-on-hover offset-y>
           <template v-slot:activator="{ on, attrs }">
             <v-btn
               text
@@ -125,40 +18,22 @@
           </template>
 
           <v-list>
-            <v-menu
-              v-model="menu"
-              :close-on-content-click="false"
-              :nudge-width="200"
-              offset-x
-              open-on-hover
+            <v-list-item
+              v-for="n in 12"
+              :key="n"
+              v-on="on"
+              @click="menu = true"
             >
-              <template v-slot:activator="{ on }">
-                <v-list-item
-                  v-for="n in 12"
-                  :key="n"
-                  v-on="on"
-                  @click="menu = true"
-                >
-                  <v-list-item-title>Test</v-list-item-title>
-
-                  <v-list-item-action>
-                    <v-icon color="black">mdi-chevron-right</v-icon>
-                  </v-list-item-action>
-                </v-list-item>
-              </template>
-
-              <v-list-item v-for="n in 12" :key="n">
+              <v-list-item-content>
                 <v-list-item-title>Test</v-list-item-title>
+              </v-list-item-content>
 
-                <v-list-item-action>
-                  <v-icon color="black">mdi-chevron-right</v-icon>
-                </v-list-item-action>
-              </v-list-item>
-            </v-menu>
-
-            end of list item
+              <v-list-item-action>
+                <v-icon color="black">mdi-chevron-right</v-icon>
+              </v-list-item-action>
+            </v-list-item>
           </v-list>
-        </v-menu> -->
+        </v-menu>
         <SearchBar></SearchBar>
         <div class="pl-2 pr-2"></div>
         <div class="d-flex flex-row mt-2">
--- src/components/navbar/main/index.vue
+++ src/components/navbar/main/index.vue
@@ -3,45 +3,100 @@
     <v-container>
       <v-row align="center" align-content="stretch" dense>
         <template v-if="width >= 900">
-          <router-link to="/" class="navbar-brand tersier--text text-h3" style="text-decoration: none; color: inherit;">
+          <router-link
+            to="/"
+            class="navbar-brand tersier--text text-h3"
+            style="text-decoration: none; color: inherit;"
+          >
             RENTARU
           </router-link>
         </template>
-<!--        <template v-else>-->
-<!--          <router-link to="/">-->
-<!--            <v-img :src="homeIcon"/>-->
-<!--          </router-link>-->
-<!--        </template>-->
-        <!--        <h1 class="tersier&#45;&#45;text" onclick="window.location.href = '/'">RENTARU</h1>-->
+
+        <v-menu open-on-hover offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              text
+              class="px-4 tersier--text font-weight-medium"
+              v-bind="attrs"
+              v-on="on"
+              >Kategori</v-btn
+            >
+          </template>
+
+          <v-card width="100vw" class="mx-auto">
+            <v-container>
+              <v-row>
+                <!-- main kategori -->
+                <v-col cols="2" class="border">
+                  <v-list>
+                    <v-list-item-group v-model="selectedMenu">
+                      <v-list-item
+                        v-for="kat in kategori"
+                        :key="kat.id"
+                        @mouseover="selectedMenu = kat.id"
+                      >
+                        <v-list-item-content>
+                          <v-list-item-title>{{ kat.text }}</v-list-item-title>
+                        </v-list-item-content>
+
+                        <v-list-item-action>
+                          <v-icon color="black">mdi-chevron-right</v-icon>
+                        </v-list-item-action>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </v-col>
+                <!-- end of main kategori -->
+
+                <!-- subkat -->
+                <v-col>
+                  <v-list>
+                    <v-list-item
+                      v-for="subkat in subkat[selectedMenu]"
+                      :key="subkat.id"
+                    >
+                      <v-list-item-content>
+                        <v-list-item-title>{{ subkat.text }}</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-col>
+                <!-- end of subkat -->
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-menu>
+        <!-- menu -->
+
         <div class="pl-2 pr-2"></div>
         <template v-if="width <= 600">
           <div style="width: 65vw;">
-            <SearchBar/>
+            <SearchBar />
           </div>
         </template>
         <template v-else>
-          <SearchBar/>
+          <SearchBar />
         </template>
-        <div style="width: 1vw;"/>
-<!--        <div class="pl-2 pr-2"></div>-->
+        <div style="width: 1vw;" />
+        <!--        <div class="pl-2 pr-2"></div>-->
         <div class="d-flex flex-row">
+          <!--          <div class="pl-2 pr-2"></div>-->
 
-<!--          <div class="pl-2 pr-2"></div>-->
-
-          <IconButton v-for="item in getData('iconButton', user.loggedIn)"
-                      :key="item.key"
-                      :icon="item.icon"
-                      :href="item.href"
+          <IconButton
+            v-for="item in getData('iconButton', user.loggedIn)"
+            :key="item.key"
+            :icon="item.icon"
+            :href="item.href"
           ></IconButton>
-          <div style="width: 1vw;"/>
+          <div style="width: 1vw;" />
 
           <Button
-              v-for="item in getData('button', user.loggedIn)"
-              :key="item.text"
-              :text="item.text"
-              :href="item.href">
+            v-for="item in getData('button', user.loggedIn)"
+            :key="item.text"
+            :text="item.text"
+            :href="item.href"
+          >
           </Button>
-
         </div>
       </v-row>
     </v-container>
@@ -52,24 +107,24 @@
 import Button from "./button";
 import IconButton from "./icon-button";
 import SearchBar from "./search-bar";
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   name: "HeaderMain",
   components: {
     Button,
     IconButton,
-    SearchBar
+    SearchBar,
   },
   data: () => ({
-    homeIcon: require('@/assets/home-32.png'),
+    homeIcon: require("@/assets/home-32.png"),
     iconButtons: [
       {
         key: "jahbfhkdafa",
         icon: "mdi-account",
         href: "/profile",
         need_auth: true,
-      }
+      },
     ],
     buttons: [
       {
@@ -86,27 +141,59 @@ export default {
         text: "Pasang Iklan",
         href: "/",
         need_auth: true,
-      }
-    ]
+      },
+    ],
+    selectedMenu: 0,
+    kategori: [
+      { id: 0, text: "kategori 1" },
+      { id: 1, text: "kategori 2" },
+      { id: 2, text: "kategori 3" },
+    ],
+    subkat: [
+      [
+        { id: 0, text: "kategori 1.1" },
+        { id: 1, text: "kategori 1.2" },
+        { id: 2, text: "kategori 1.3" },
+        { id: 3, text: "kategori 1.4" },
+        { id: 4, text: "kategori 1.5" },
+      ],
+      [
+        { id: 0, text: "kategori 2.1" },
+        { id: 1, text: "kategori 2.2" },
+        { id: 2, text: "kategori 2.3" },
+        { id: 3, text: "kategori 2.4" },
+        { id: 4, text: "kategori 2.5" },
+      ],
+      [
+        { id: 0, text: "kategori 3.1" },
+        { id: 1, text: "kategori 3.2" },
+        { id: 2, text: "kategori 3.3" },
+        { id: 3, text: "kategori 3.4" },
+        { id: 4, text: "kategori 3.5" },
+      ],
+    ],
   }),
   methods: {
     getData(param, authStatus) {
       if (param === "iconButton") {
-        return this.iconButtons.filter(value => value.need_auth === authStatus);
+        return this.iconButtons.filter(
+          (value) => value.need_auth === authStatus
+        );
       } else if (param === "button") {
-        return this.buttons.filter(value => value.need_auth === authStatus);
+        return this.buttons.filter((value) => value.need_auth === authStatus);
       } else {
         return null;
       }
     },
   },
+
   computed: {
     width() {
       return screen.width;
     },
     ...mapGetters({
-      user: "user"
-    })
+      user: "user",
+    }),
   },
 };
 </script>
